a3f7d9f24f1b83b6b4f125d57f84c9f7e1ac4982
Dee <deeit789@gmail.com>
2022-02-19 21:55:44 +0700

    .

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moto88 - Giới Thiệu Người Chơi</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="./images/favicon-75.png">
+
+
+    <link rel="stylesheet" href="./css/reset.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="./css/style.css?<?php echo rand(1, 999999) ?>" rel="stylesheet" type="text/css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+
+<body>
+
+    <div class="banner">
+        <img src="./images/banner.png" alt="">
+    </div>
+    <h1 class="title">HƯỚNG DẪN GIỚI THIỆU THÀNH VIÊN</h1>
+    <div class="wrapper">
+        <div class="container-content">
+            <div class="box-tutorial">
+                <div class="list-tutorial">
+                    <div class="item-tutorial">
+                        <img src="./images/b1.png" alt="">
+                    </div>
+                    <div class="item-tutorial">
+                        <img src="./images/b2.png" alt="">
+                    </div>
+                    <div class="item-tutorial">
+                        <img src="./images/b3.png" alt="">
+                    </div>
+                    <div class="item-tutorial">
+                        <img src="./images/b4.png" alt="">
+                    </div>
+                    <div class="item-tutorial">
+                        <img src="./images/b5.png" alt="">
+                    </div>
+                    <div class="item-tutorial">
+                        <img src="./images/b6.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg">
+        <div class=" wrapper box-benefit">
+            <div class="item-benefit">
+                <p class="title-benefit">Doanh thu trong ngày</p>
+                <p class="sub-benefit">1 điểm</p>
+            </div>
+            <div class="item-benefit">
+                <p class="title-benefit">Tỷ lệ hoa hồng</p>
+                <p class="sub-benefit">0.2%</p>
+            </div>
+            <div class="item-benefit">
+                <p class="title-benefit">Giới hạn tiền hoa hồng</p>
+                <p class="sub-benefit">Không giới hạn</p>
+            </div>
+            <div class="item-benefit">
+                <p class="title-benefit">Vòng cược</p>
+                <p class="sub-benefit">1 vòng</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper">
+        <div class="content">
+            <h2 class="title-content">ĐỐI TƯỢNG: TOÀN BỘ THÀNH VIÊN CỦA MOTO88</h2>
+
+            <p>*Trở thành Người giới thiệu MOTO88 không yêu cầu bất kỳ khoản phí nào, bạn có thể bắt đầu kiếm tiền không
+                giới hạn, cho dù bạn có tài nguyên mạng hay không có tài nguyên mạng, cho dù bạn đang ở nhà hay đang đi
+                công tác, bạn có thể dễ dàng kiếm tiền mọi lúc mọi nơi.</p>
+
+            <p>* Có 5 cấp độ Người giới thiệu trong hệ thống đa cấp, Người giới thiệu có thể được mở rộng đến nhiều chi
+                nhánh và người giới thiệu hơn. Phát triển bản thân và nhận 0,2% hoa hồng dựa trên mức cược hiệu quả của
+                những người bạn được giới thiệu.</p>
+
+            <p>* Ước mơ tỷ lệ hoa hồng cao nhất thị trường, khấu trừ thấp, thu nhập hàng tháng hơn 100 triệu cũng không
+                có gì đáng ngạc nhiên.</p>
+
+            <p>* Người được giới thiệu có thể tham gia vào liên kết giới thiệu này miễn là họ có tài khoản đăng ký tại
+                MOTO88.</p>
+
+            <p>* Chỉ cần bạn muốn là Người giới thiệu của chúng tôi, chỉ cần bạn có liên hệ, bạn không phải làm gì cả,
+                và sẽ không mất quá nhiều thời gian. Tất cả những gì bạn phải làm là liên hệ với bộ phận chăm sóc khách
+                hàng 24/7 để được hỗ trợ và tư vấn.</p>
+
+            <p>* Kinh doanh không rủi ro, lợi nhuận cao, trả hoa hồng hàng ngày. Hoa hồng không liên tục MOTO88 dựa vào
+                số tiền cá cược hiệu quả của cấp dưới để tính hoa hồng Hệ thống sẽ cập nhật trực tiếp vào tài khoản GAME
+                sau 6h sáng hàng ngày để đáp ứng nhu cầu đa đối tượng của khách hàng.</p>
+
+            <p>* MOTO88 đã hoạt động nhiều năm và được hàng triệu người chơi tin tưởng. Thông qua nhiều năm quảng bá
+                thương hiệu mạnh mẽ và uy tín, người giới thiệu có thể được hưởng lợi từ việc quảng bá thương hiệu của
+                chúng tôi.</p>
+
+            <p>* Lợi nhuận được tính bằng tổng số tiền cá cược của các thành viên trực tuyến, tỷ lệ hoa hồng cao nhất
+                thị trường, không giới hạn số lượng thành viên.</p>
+
+            <p>* Chúng tôi cung cấp các trò chơi hấp dẫn, đa dạng: điện tử, người chơi thật, thể thao, câu cá, cờ tướng,
+                xổ số, đá gà ...</p>
+
+            <p>
+                * Giới thiệu bạn bè trở thành người giới thiệu để nhận hoa hồng giới thiệu không giới hạn.
+            </p>
+
+            <img src="./images/content.png" alt="">
+
+            <p>* LƯU Ý: Khi bạn chia sẻ cho bạn của bạn và họ chia sẻ cho bạn bè của họ. Vậy bạn của bạn sẽ trở thành
+                (N). Như vậy là bạn có thể cùng bạn bè cùng nhau kiếm tiền.
+            </p>
+            <p>
+                * Nếu phát hiện thấy thành viên nào có hành vi gian lận tiền hoa hồng, hệ thống có quyền hủy tài khoản
+                và tịch thu số dư tài khoản.
+            </p>
+
+            <p>* Mỗi người chỉ có thể đăng ký 1 tài khoản duy nhất trên nền tảng.</p>
+
+            <p>* Thành viên phải tuân thủ nghiêm ngặt các điều khoản sử dụng của MOTO88. Nếu phát hiện có hành vi lạm
+                dụng, MOTO88 có quyền hủy bỏ khuyến mãi này cho tất cả người chơi hoặc thành viên, nhóm / tổ chức vào
+                bất
+                kỳ lúc nào và do chúng tôi quyết định.
+            </p>
+            <p>Chi tiết Hoạt động:
+            </p>
+            <p>1. Tiền hoàn trả chỉ cần trải qua 1 vòng cược có thể rút tiền ngay, Tất cả các trò chơi trên bàn điện tử,
+                cược hòa, cược vô hiệu, và cược đối đầu 2 bên không được tính là cược hợp lệ.
+            </p>
+            <p>
+                2. Nếu thành viên có tranh chấp về sự kiện, nhằm đảm bảo quyền lợi của hai bên và ngăn chặn việc đánh
+                cắp danh tính. Giải trí MOTO88 có quyền yêu cầu thành viên cung cấp cho chúng tôi các tài liệu liên quan
+                xác thực bạn có đủ điều kiện để nhận khuyên mãi.
+            </p>
+            <p>3. Mỗi thành viên chỉ được nhận khuyến mãi một lần/ngày, nếu trùng tên, số điện thoại, IP thì coi như
+                cùng một người, và công ty có quyền từ chối phát khuyến mãi.
+            </p>
+            <p>
+                4. Nếu bạn quên tài khoản / mật khẩu thành viên, vui lòng liên hệ với bộ phận chăm sóc khách hàng trực
+                tuyến 24/7 của MOTO88 để được giúp đỡ lấy lại thông tin tài khoản.
+
+            </p>
+            <p style="padding-bottom: 2rem;">
+                5. Tham gia ưu đãi có nghĩa là bạn đồng ý với "Quy tắc và Điều khoản ưu đãi".
+            </p>
+        </div>
+    </div>
+    <script>
+
+
+    </script>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
